acbd979b0a6a27288ad9b250586790a5ea45e66f
gfx <gfuji@cpan.org>
2010-09-26 13:45:21 +0900

    Checking in changes prior to tagging of version 0.002.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 19e8c62..8ac37ef 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension MouseX::Extend
    
    +0.002 Sun Sep 26 13:44:40 2010
    +    - No code changes
    +    - Fix packaging issues
    +
     0.001
         - original version; created by Module::Setup
           at Fri Feb 26 22:24:38 2010.

--- lib/MouseX/Foreign/Meta/Role/Method/Destructor.pm
+++ lib/MouseX/Foreign/Meta/Role/Method/Destructor.pm
@@ -32,7 +32,7 @@ MouseX::Foreign::Meta::Role::Method::Destructor - The MouseX::Foreign meta metho
 
 =head1 VERSION
 
-This document describes MouseX::Foreign version 0.001.
+This document describes MouseX::Foreign version 0.002.
 
 =head1 DESCRIPTION
 
