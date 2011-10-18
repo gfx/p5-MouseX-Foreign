afca1abdc71d414829263c42e9a49e5eccfc5f67
Fuji Goro <fuji.goro@dena.jp>
2011-10-17 18:58:24 -0700

    Checking in changes prior to tagging of version 0.007.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 34bf608..520888e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension MouseX::Foreign
    
    +0.007 2011-10-17 18:57:02
    +    - Declare Any::Moose version (rsimoes)
    +    - Fix duplicated code in meta methods (rsimoes)
    +
     0.006 2011-10-13 10:47:07
         - Add MouseX::NonMoose for drop-in compatibility with Any::Moose
         - Add test files for new module

--- lib/MouseX/Foreign/Meta/Role/Method/Destructor.pm
+++ lib/MouseX/Foreign/Meta/Role/Method/Destructor.pm
@@ -32,7 +32,7 @@ MouseX::Foreign::Meta::Role::Method::Destructor - The MouseX::Foreign meta metho
 
 =head1 VERSION
 
-This document describes MouseX::Foreign version 0.006.
+This document describes MouseX::Foreign version 0.007.
 
 =head1 DESCRIPTION
 
